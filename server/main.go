8512cf9589fd904779b007d479827ceb29a0c547
Савелий Карпухин <sakarpukhin@sberbank.ru>
2022-12-15 15:54:38 +0300

    Added Hello World with gin

---
+++ server/main.go
@@ -0,0 +1,17 @@
+package main
+
+import (
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	gin.SetMode(gin.DebugMode)
+
+	r := gin.Default()
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, gin.H{
+			"message": "Hello World!",
+		})
+	})
+	r.Run()
+}
